d4d8ce7fb9ebbcbd79617961bd9cf05a812af330
SOLSD <alfie.e.jones@hotmail.co.uk>
2021-11-18 00:59:49 +0000

    Finished tally function and get_score function

--- algo_work/program_files/tallying_parties.py
+++ algo_work/program_files/tallying_parties.py
@@ -1,11 +1,19 @@
-"""This is the file for where all code relating to the tallying up of parties shall go"""
-def tally(mwcs):
-    List_of_lists = [item for t in mwcs for item in t]
-    print(List_of_lists)
-    for i in range(len(List_of_lists)):
-            count = List_of_lists.count(List_of_lists[i])
-            print(List_of_lists[i], count)
 
-# TODO - Find a way to store previously tallied parties so as to not end up with duplicate counts.
+def tally(mwcs):
+    list_of_lists = [item for t in mwcs for item in t]
+    storage = []
+    denominator = len(list_of_lists)
+
+    for i in range(len(list_of_lists)):
+        if storage.count(list_of_lists[i]) == 0:
+            storage.append(list_of_lists[i])
+            count = list_of_lists.count(list_of_lists[i])
+            get_score(list_of_lists[i][0], count, denominator)
+
+
+def get_score(party_name, count, denominator):
+    print("Score for party ", party_name, " is ", (count/denominator))
+
+
 
 tally([[('B', 5), ('C', 3), ('D', 4), ('E', 3)], [('D', 4), ('E', 3), ('F', 8)]])
