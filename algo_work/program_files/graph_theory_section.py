d5692c369d25f14f0c26195b3260c3cf8a177c28
SOLSD <alfie.e.jones@hotmail.co.uk>
2021-11-22 18:21:47 +0000

    Started on the conductance function. The internal strength can now be calculated.

--- algo_work/program_files/graph_theory_section.py
+++ algo_work/program_files/graph_theory_section.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools as it
 
 
 def set_up_prob_matrix(parties):
@@ -12,7 +13,32 @@ def set_up_prob_matrix(parties):
                 statement = "Enter probability for (", parties[i][0], parties[j][0], ")"
                 prob = input(statement)  # User input to give probability to appropriate indices in matrix
                 prob_matrix[i][j] = prob
+    return prob_matrix
+
+
+def conductance(parties, mwcs, prob_matrix):
+    # everything out over everything out+double everything in
+    '''
+    for each mwc in mwcs:
+        sum probs between parties and double
+        sum probs that have one end in mwc
+        psi = latter/former+latter
+        put psi in list with mwc as tuple
+        '''
+    # Everything in
+    for mwc in mwcs:
+        vertices = list(it.combinations(mwc, 2))
+        everything_in = 0
+        for vertex in vertices:
+            row = parties.index(vertex[0])
+            column = parties.index(vertex[1])
+            everything_in += prob_matrix[row][column]
+            print("Vertex:", vertex, "; Row:", row, "; Column:", column, "; To add:", prob_matrix[row][column], "; New everything_in:", everything_in)
     print(prob_matrix)
+    print(everything_in)
+
+    # Everything out
 
 
-set_up_prob_matrix([('A', 2), ('B', 5), ('C', 3), ('D', 4), ('E', 3), ('F', 8)])
+prob_matrix = set_up_prob_matrix([('A', 2), ('B', 5), ('C', 3), ('D', 4)])
+conductance([('A', 2), ('B', 5), ('C', 3), ('D', 4)], [[('A', 2), ('B', 5), ('C', 3)]], prob_matrix)
