5e65df33decf642559393236b5c1cbb611f0ff01
SOLSD <alfie.e.jones@hotmail.co.uk>
2021-11-23 16:18:13 +0000

    External pull calculation now working. Had to make prob matrix symmetrical.

--- algo_work/program_files/graph_theory_section.py
+++ algo_work/program_files/graph_theory_section.py
@@ -13,6 +13,7 @@ def set_up_prob_matrix(parties):
                 statement = "Enter probability for (", parties[i][0], parties[j][0], ")"
                 prob = input(statement)  # User input to give probability to appropriate indices in matrix
                 prob_matrix[i][j] = prob
+                prob_matrix[j][i] = prob
     return prob_matrix
 
 
@@ -25,20 +26,33 @@ def conductance(parties, mwcs, prob_matrix):
         psi = latter/former+latter
         put psi in list with mwc as tuple
         '''
-    # Everything in
+    print(prob_matrix)
     for mwc in mwcs:
+        # Internal Strength
+        print("-----------------")
+        print("INTERNAL STRENGTH")
+        print("-----------------")
         vertices = list(it.combinations(mwc, 2))
         everything_in = 0
+        everything_out = 0
         for vertex in vertices:
             row = parties.index(vertex[0])
             column = parties.index(vertex[1])
             everything_in += prob_matrix[row][column]
             print("Vertex:", vertex, "; Row:", row, "; Column:", column, "; To add:", prob_matrix[row][column], "; New everything_in:", everything_in)
-    print(prob_matrix)
-    print(everything_in)
-
-    # Everything out
+        print(everything_in)
+        # External Pull
+        print("-------------")
+        print("EXTERNAL PULL")
+        print("-------------")
+        for party in mwc:
+            row = parties.index(party)
+            for i in range(len(parties)):
+                if prob_matrix[row][i] != 0 and parties[i] not in mwc:
+                    everything_out += prob_matrix[row][i]
+                    print("Added", prob_matrix[row][i], "External pull now", everything_out)
+        print(everything_out)
 
 
 prob_matrix = set_up_prob_matrix([('A', 2), ('B', 5), ('C', 3), ('D', 4)])
-conductance([('A', 2), ('B', 5), ('C', 3), ('D', 4)], [[('A', 2), ('B', 5), ('C', 3)]], prob_matrix)
+conductance([('A', 2), ('B', 5), ('C', 3), ('D', 4)], [[('A', 2), ('B', 5), ('C', 3), ('D', 4)], [('A', 2), ('C', 3)], [('A', 2), ('B', 5), ('D', 4)]], prob_matrix)
