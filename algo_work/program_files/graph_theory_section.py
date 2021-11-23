a9595dc07b5ba4fd7cd1fac7db8db53350caa417
SOLSD <alfie.e.jones@hotmail.co.uk>
2021-11-23 16:32:38 +0000

    Added final calculation to find contuctance of each mwc

--- algo_work/program_files/graph_theory_section.py
+++ algo_work/program_files/graph_theory_section.py
@@ -13,34 +13,35 @@ def set_up_prob_matrix(parties):
                 statement = "Enter probability for (", parties[i][0], parties[j][0], ")"
                 prob = input(statement)  # User input to give probability to appropriate indices in matrix
                 prob_matrix[i][j] = prob
-                prob_matrix[j][i] = prob
+                prob_matrix[j][i] = prob  # Makes matrix symmetrical
     return prob_matrix
 
 
 def conductance(parties, mwcs, prob_matrix):
-    # everything out over everything out+double everything in
-    '''
-    for each mwc in mwcs:
-        sum probs between parties and double
-        sum probs that have one end in mwc
-        psi = latter/former+latter
-        put psi in list with mwc as tuple
-        '''
+    """
+    Function calculates the Conductance of the probability matrix.
+
+    Calculates the internal strength and external pull of the mwc then divides the latter by the sum of both.
+    """
     print(prob_matrix)
+    conductances = []
     for mwc in mwcs:
+
         # Internal Strength
         print("-----------------")
         print("INTERNAL STRENGTH")
         print("-----------------")
-        vertices = list(it.combinations(mwc, 2))
+        vertices = list(it.combinations(mwc, 2))  # Creates all vertex pairings of the mwc in the graph
         everything_in = 0
         everything_out = 0
         for vertex in vertices:
-            row = parties.index(vertex[0])
-            column = parties.index(vertex[1])
+            row = parties.index(vertex[0])  # Since prob matrix and parties have the same ordering of the parties...
+            column = parties.index(vertex[1])  # ...this lines up the rows and columns.
             everything_in += prob_matrix[row][column]
-            print("Vertex:", vertex, "; Row:", row, "; Column:", column, "; To add:", prob_matrix[row][column], "; New everything_in:", everything_in)
+            print("Vertex:", vertex, "; Row:", row, "; Column:", column, "; To add:", prob_matrix[row][column],
+                  "; New everything_in:", everything_in)
         print(everything_in)
+
         # External Pull
         print("-------------")
         print("EXTERNAL PULL")
@@ -48,11 +49,19 @@ def conductance(parties, mwcs, prob_matrix):
         for party in mwc:
             row = parties.index(party)
             for i in range(len(parties)):
-                if prob_matrix[row][i] != 0 and parties[i] not in mwc:
+                if prob_matrix[row][i] != 0 and parties[i] not in mwc:  # Checks if the column currently being looked at
+                    # is associated with a party not in the mwc and is > 0
                     everything_out += prob_matrix[row][i]
                     print("Added", prob_matrix[row][i], "External pull now", everything_out)
         print(everything_out)
 
+        # Final conductance
+        psi = (everything_out / (everything_out + (2 * everything_in)))
+        conductances.append((mwc, psi))
+    print(conductances)
+    return conductances
+
 
 prob_matrix = set_up_prob_matrix([('A', 2), ('B', 5), ('C', 3), ('D', 4)])
-conductance([('A', 2), ('B', 5), ('C', 3), ('D', 4)], [[('A', 2), ('B', 5), ('C', 3), ('D', 4)], [('A', 2), ('C', 3)], [('A', 2), ('B', 5), ('D', 4)]], prob_matrix)
+conductance([('A', 2), ('B', 5), ('C', 3), ('D', 4)], [[('A', 2), ('B', 5), ('C', 3), ('D', 4)], [('A', 2), ('C', 3)],
+                                                       [('A', 2), ('B', 5), ('D', 4)]], prob_matrix)
