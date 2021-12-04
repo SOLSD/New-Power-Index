94d92142189efe15d413eb6ad9c110c6c7961490
solsd <alfie.e.jones@hotmail.co.uk>
2021-12-04 19:36:31 +0000

    Noticed a small bug that caused incorrect scores if lists were not in order so fixed by sorting list_of_lambdas and tally before calculating

--- algo_work/program_files/main.py
+++ algo_work/program_files/main.py
@@ -13,7 +13,7 @@ def run():
         statement = "Enter number of votes the party has: "
         number_of_votes = int(input(statement))
         parties.append((name_of_party, number_of_votes))
-    print(parties)
+
     statement = "Enter the number of votes needed to win: "
     target = int(input(statement))
     prob_matrix = graph_theory_section.set_up_prob_matrix(parties)
@@ -22,6 +22,8 @@ def run():
     ceps = graph_theory_section.cep(parties, list_of_mwcs, prob_matrix)
     tally = tallying_parties.tally(list_of_mwcs)
     list_of_lambdas = all_the_lambda.assign_Lambda(parties, conductances, ceps)
+    tally = sorted(tally)
+    list_of_lambdas = sorted(list_of_lambdas)
     unnormalised_scores = (all_the_lambda.score_lambda_fun_times(tally, list_of_lambdas))
     all_the_lambda.normalise(unnormalised_scores)
 
