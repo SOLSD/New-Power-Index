63485ec085319d0e3550e5fae66cfcade4826d46
solsd <alfie.e.jones@hotmail.co.uk>
2022-01-02 01:34:45 +0000

    Added edge case code for if a party receives more votes than target

---
+++ algo_work/program_files/edge_cases.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def party_hits_target_check(parties, target):
+    for i in range(len(parties)):
+        if parties[i][-1] >= target:
+            return i
+    return -1
+
+
+def winning_party(parties, index):
+    final_scores = []
+    for i in range(len(parties)):
+        name = parties[i][0]
+        if i == index:
+            final_scores.append((name, 1))
+        else:
+            final_scores.append((name, 0))
+    return final_scores
+
--- algo_work/program_files/main.py
+++ algo_work/program_files/main.py
@@ -1,4 +1,4 @@
-from algo_work.program_files import all_the_lambda, find_mwc, graph_theory_section, tallying_parties
+from algo_work.program_files import all_the_lambda, find_mwc, graph_theory_section, tallying_parties, edge_cases
 
 statement = "Enter number of parties: "
 number_of_parties = int(input(statement))
@@ -16,16 +16,21 @@ def run():
 
     statement = "Enter the number of votes needed to win: "
     target = int(input(statement))
-    prob_matrix = graph_theory_section.set_up_prob_matrix(parties)
-    list_of_mwcs = find_mwc.generate_coalitions(parties, target)
-    conductances = graph_theory_section.conductance(parties, list_of_mwcs, prob_matrix)
-    ceps = graph_theory_section.cep(parties, list_of_mwcs, prob_matrix)
-    tally = tallying_parties.tally(list_of_mwcs)
-    list_of_lambdas = all_the_lambda.assign_Lambda(parties, conductances, ceps)
-    tally = sorted(tally)
-    list_of_lambdas = sorted(list_of_lambdas)
-    unnormalised_scores = (all_the_lambda.score_lambda_fun_times(tally, list_of_lambdas))
-    all_the_lambda.normalise(unnormalised_scores)
+    target_check = edge_cases.party_hits_target_check(parties, target)
+    if target_check > -1:
+        print("The final results are:")
+        print(edge_cases.winning_party(parties, target_check))
+    else:
+        prob_matrix = graph_theory_section.set_up_prob_matrix(parties)
+        list_of_mwcs = find_mwc.generate_coalitions(parties, target)
+        conductances = graph_theory_section.conductance(parties, list_of_mwcs, prob_matrix)
+        ceps = graph_theory_section.cep(parties, list_of_mwcs, prob_matrix)
+        tally = tallying_parties.tally(list_of_mwcs)
+        list_of_lambdas = all_the_lambda.assign_Lambda(parties, conductances, ceps)
+        tally = sorted(tally)
+        list_of_lambdas = sorted(list_of_lambdas)
+        unnormalised_scores = (all_the_lambda.score_lambda_fun_times(tally, list_of_lambdas))
+        all_the_lambda.normalise(unnormalised_scores)
 
 
 run()
