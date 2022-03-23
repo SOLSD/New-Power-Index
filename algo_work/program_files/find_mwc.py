40ab62168c9c6316ad192b3977db96616b4f7399
solsd <alfie.e.jones@hotmail.co.uk>
2022-03-23 16:08:33 +0000

    Bug in prob matrix code fixed.

---
+++ algo_work/program_files/find_mwc.py
@@ -0,0 +1,68 @@
+from itertools import combinations
+import graph_theory_section, all_the_lambda, tallying_parties
+
+
+def find_min_coalition(parties, target):
+    """
+    Function to find minimum number of parties needed to make an MWC
+    """
+    votes = []
+    min_start = 0
+    for party in parties:
+        votes.append(party[0])  # Adds party votes to list
+    votes = sorted(votes)  # Puts list in numerical order
+    total = 0
+    for i in range(len(votes) + 1):
+        total += votes[-i]  # Adds party votes together and checks if it's more than target
+        if total >= target:
+            min_start = i  # If total is more than target then that is min number of parties needed
+            break
+    return min_start
+
+
+def generate_coalitions(parties, target, starting_point, prob_matrix):
+    """
+    Finds all combinations needed from parties
+
+    Takes the list of parties and cycles through,
+    creating a list of all winning combinations.
+
+    """
+
+    for i in range(starting_point, len(parties)+1):
+        all_combos = combinations(parties, i)  # generates all combinations of length i from parties list
+        for combo in all_combos:
+            total = 0
+            for j in range(len(combo)):
+                total += combo[j][0]  # Calculates total votes for each coalition
+            if total < target:
+                continue
+            if minimal_test(combo, total, target):  # Checks if coalition is M and W
+
+                print(combo)
+
+                psi = graph_theory_section.conductance(parties, combo, prob_matrix)  # Finds conductance of MWC
+                cep = graph_theory_section.cep(parties, combo, prob_matrix)  # Finds existence probability of MWC
+                Lambda = all_the_lambda.assign_Lambda(combo, psi, cep)  # Finds lambda value of MWC
+                tally = tallying_parties.tally_up(combo)
+    return (Lambda, tally)
+
+
+
+def minimal_test(combination, total, target):
+    """
+    Takes combination and checks if it is minimal
+
+    The each party's votes are removed from the total one at a time
+    and if for all parties, each time that happens the total is
+    smaller than the target, the combination is minimal.
+    """
+    count = 0
+    for i in range(len(combination)):
+        check = total - combination[i][0]  # Removes one party from coalition one at a time to check if minimal
+        if check < target:
+            count += 1  # Every time the coalitions total falls under the target is counted.
+    if count == len(combination) and total >= target:  # If the removal of every party in the coalition results in the
+        # rest not reaching the target then the coalition is minimal
+        return True
+    return False
---
+++ algo_work/program_files/main.py
@@ -0,0 +1,86 @@
+#from algo_work.program_files import all_the_lambda, find_mwc, graph_theory_section, tallying_parties, edge_cases, display_graphs
+import all_the_lambda, find_mwc, graph_theory_section, tallying_parties, edge_cases, display_graphs
+import sys
+
+def new_run():
+    """
+    Runs the program
+    """
+    # --------------------------------------------------------
+    """
+    Gets location of needed information from user.
+    """
+    argv = sys.argv[1:]
+    print(argv)
+    party_location = str(argv[0])
+    prob_location = str(argv[1])
+    num_parties = int(argv[2])
+    target = int(argv[3])
+    # --------------------------------------------------------
+    """
+    Loads and reads file for parties and votes information.
+    """
+    parties = []
+    fileobj = open(party_location)
+    parties_and_votes = fileobj.readlines()
+    parties_and_votes = [line.strip() for line in parties_and_votes]
+    for i in range(0, num_parties * 2, 2):  # Takes each pair of party name and votes and assigns them as a tuple to
+        # the parties list
+        name = parties_and_votes[i]
+        votes = int(parties_and_votes[i + 1])
+        parties.append((votes, name))
+    print(parties)
+    parties = sorted(parties)
+    parties.reverse()
+    print(parties)
+    # --------------------------------------------------------
+    """
+    Checks for edge cases
+    """
+    if edge_cases.same_name_check(parties):
+        new_run()
+
+    if edge_cases.can_reach_target(parties, target):
+        print("Sum of votes doesn't reach target. Reduce target required")
+        quit()
+
+    if edge_cases.sum_is_target_check(parties, target):
+        final_scores = []
+        for i in range(len(parties)):
+            name = parties[i][0]
+            final_scores.append((name, 1/len(parties)))
+        print("The final results are:")
+        print(final_scores)
+        quit()
+
+    target_check = edge_cases.party_hits_target_check(parties, target)
+    if target_check > -1:
+        print("The final results are:")
+        print(edge_cases.winning_party(parties, target_check))
+    # --------------------------------------------------------
+    """
+    Set up the probability matrix
+    """
+    prob_matrix = graph_theory_section.set_up_prob_matrix(parties, prob_location)
+    print("Matrix Complete!")
+    # --------------------------------------------------------
+    """
+    Minimal Winning Coalition -> Score loop
+    """
+    starting_point = find_mwc.find_min_coalition(parties, target)
+    print("Smallest coalitions consist of %d parties" % starting_point)
+    l_and_t = find_mwc.generate_coalitions(parties, target, starting_point, prob_matrix)
+    scores = tallying_parties.final_tally(l_and_t[-1])
+    lambdas = l_and_t[0]
+    unnormalised_scores = all_the_lambda.score_lambda_fun_times(scores, lambdas)
+    print("---------------------------------------------------------------------")
+    normalised_scores = all_the_lambda.normalise(unnormalised_scores)
+    print("---------------------------------------------------------------------")
+    display_graphs.plot_graph(normalised_scores)
+
+
+new_run()
+
+
+
+
